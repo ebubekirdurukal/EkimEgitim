a2d5614361ad46643c8884e878ef1518a51e49f2
ebubekir <ebubekir.durukal@testinium.com>
2022-10-29 12:32:21 +0300

    Initial Commit

---
+++ src/main/java/com/egitim/proje/First.java
@@ -0,0 +1,25 @@
+package com.egitim.proje;
+
+public class First {
+
+    // bu bir one line comment line'dır
+
+    /*
+        Bu bir
+        multiline comment line'dır(açıklama satırı)
+     */
+
+    /**
+     * @date October 22nd 2022
+     * @author Ebubekir Durukal
+     * @param args
+     * Bu bir Javadoc yorum bölümüdür.
+     */
+    public static void main(String[] args) {  // main kısayolu
+        System.out.println("Merhaba Dünya");  // sout kısayolu
+        System.out.println("bu da ikinci yazdığım kod");
+    }
+
+
+
+}
